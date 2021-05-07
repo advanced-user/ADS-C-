8c5666ad82827f55903b5803c5c02b6d34b6109e
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-07 19:22:37 +0300

    Implemented the main functionality of the queue

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -109,6 +109,135 @@ void List<T>::HeapSort()
 }
 
 
+template <typename T>
+class PriorityQueue
+{
+public:
+    PriorityQueue();
+    ~PriorityQueue();
+
+    int getSize() { return size;}
+    bool isEmpty();
+    void printQueue();
+    void enqueue (T data_priority, T data_value);
+    void dequeueMax();
+
+private:
+    template<typename T>
+    struct Node{
+        T data_priority;
+        T data_value;
+
+        Node(T data_priority, T data_value){
+            this->data_priority = data_priority;
+            this->data_value = data_value;
+        }
+    };
+
+    int size;
+    std::vector<Node<T>> list;
+
+    void swap(int index1, int index2, std::vector<Node<T>> &list);
+};
+
+template<typename T>
+PriorityQueue<T>::PriorityQueue() {
+    size = 0;
+}
+
+template<typename T>
+PriorityQueue<T>::~PriorityQueue() {}
+
+template<typename T>
+bool PriorityQueue<T>::isEmpty()
+{
+    if(size > 0)
+        return false;
+    return true;
+}
+
+template<typename T>
+void PriorityQueue<T>::printQueue()
+{
+    std::vector<Node<T>> listPriorityQueue;
+    std::vector<Node<T>> a = list;
+    int n = a.size();
+    listPriorityQueue.clear();
+
+    for(int i = 0; i < n; i++)
+    {
+        listPriorityQueue.push_back(a[0]);
+        a[0] = a[a.size() - 1];
+        a.erase(a.begin() + a.size() - 1);
+
+        int j = 0;
+        int k = 0;
+        while(true)
+        {
+            if(2*j+1 < n-i-1 && a[2*j + 1].data_priority < a[k].data_priority)
+                k = 2*j + 1;
+            if(2*j+2 < n-i-1 && a[2*j + 2].data_priority < a[k].data_priority)
+                k = 2*j + 2;
+            if(k == j)
+                break;
+            swap(j, k, a);
+
+            j = k;
+        }
+    }
+
+
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << listPriorityQueue[i].data_value << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+template<typename T>
+void PriorityQueue<T>::enqueue(T dataPriority, T data_value)
+{
+    this->list.push_back(Node<T>(dataPriority, data_value));
+    for(int i = list.size() - 1; i > 0 && list[(i-1) / 2].data_priority > list[i].data_priority; i = (i - 1) / 2)
+    {
+        swap(i, (i-1) / 2, list);
+    }
+    size++;
+}
+
+template<typename T>
+void PriorityQueue<T>::dequeueMax()
+{
+    list[0] = list[list.size() - 1];
+    list.erase(list.begin() + list.size() - 1);
+    size--;
+
+    int j = 0;
+    int k = 0;
+    int n = list.size();
+    while(true)
+    {
+        if(2*j+1 < n && list[2*j + 1].data_priority < list[k].data_priority)
+            k = 2*j + 1;
+        if(2*j+2 < n && list[2*j + 2].data_priority < list[k].data_priority)
+            k = 2*j + 2;
+        if(k == j)
+            break;
+        swap(j, k, list);
+
+        j = k;
+    }
+}
+
+template<typename T>
+void PriorityQueue<T>::swap(int index1, int index2, std::vector<Node<T>> &list) {
+    Node<T> t = list[index1];
+    list[index1] = list[index2];
+    list[index2] = t;
+}
+
+
 int main()
 {
     List<int> list;
@@ -127,6 +256,21 @@ int main()
 
     list.HeapSort();
     list.printList();
+
+    PriorityQueue<int> queue;
+    queue.enqueue(5, 5);
+    queue.enqueue(3, 3);
+    queue.enqueue(10, 10);
+    queue.enqueue(40, 40);
+    queue.enqueue(12, 12);
+    queue.enqueue(9, 9);
+    queue.enqueue(7, 7);
+    queue.enqueue(20, 20);
+    queue.enqueue(27, 27);
+    queue.enqueue(25, 25);
+
+    queue.dequeueMax();
+    queue.printQueue();
 
     return 0;
 }
