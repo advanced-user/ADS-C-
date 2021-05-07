cddec1e1f61e30768975c6cc6757f381556cbe7b
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-07 15:41:38 +0300

    Added element to heap implemented

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -9,6 +9,7 @@ public:
     ~List();
 
     void push_back(T item);
+    void swap(int index1, int index2);
     bool isEmpty();
     void printList();
     void HeapSort();
@@ -31,7 +32,20 @@ List<T>::~List() = default;
 template<typename T>
 void List<T>::push_back(T item)
 {
+    list.push_back(item);
+    for(int i = list.size() - 1; i > 0 && list[(i-1) / 2] > list[i]; i = (i - 1) / 2)
+    {
+        swap(i, (i-1) / 2);
+    }
+    size++;
+}
 
+template<typename T>
+void List<T>::swap(int index1, int index2)
+{
+    T t = list[index1];
+    list[index1] = list[index2];
+    list[index2] = t;
 }
 
 template<typename T>
@@ -41,8 +55,33 @@ bool List<T>::isEmpty() {
     return false;
 }
 
+template<typename T>
+void List<T>::printList()
+{
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << list[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
-int main() {
+
+int main()
+{
+    List<int> list;
+
+    list.push_back(5);
+    list.push_back(3);
+    list.push_back(10);
+    list.push_back(12);
+    list.push_back(9);
+    list.push_back(7);
+    list.push_back(20);
+    list.push_back(27);
+    list.push_back(25);
+    list.push_back(40);
+
+    list.printList();
 
     return 0;
 }
